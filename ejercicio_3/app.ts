9b58988c9cc800e0993ef03a03f9cddeb4abbab5
DESKTOP-3MCK973\FBLAS <florenblas@gmail.com>
2017-05-17 13:31:48 +0200

    exercise_3. part_one

--- ejercicio_3/app.ts
+++ ejercicio_3/app.ts
@@ -1,13 +1,23 @@
 
 // Objetos
-var batimovil = {
+
+
+type Auto =  {
+  carroceria:string,
+  modelo:string,
+  antibalas:boolean,
+  pasajeros:number,
+  disparar?:()=>void,
+
+}
+let batimovil:Auto = {
   carroceria: "Negra",
   modelo: "6x6",
   antibalas: true,
   pasajeros:4
 };
 
-var bumblebee = {
+let bumblebee:Auto = {
   carroceria: "Amarillo con negro",
   modelo: "4x2",
   antibalas: true,
@@ -19,7 +29,7 @@ var bumblebee = {
 
 
 // Villanos debe de ser un arreglo de objetos personalizados
-var villanos = [{
+let villanos = [{
   nombre:"Lex Luthor",
   edad: 54,
   mutante:false
@@ -35,18 +45,18 @@ var villanos = [{
 
 // Multiples tipos
 // cree dos tipos, uno para charles y otro para apocalipsis
-var charles = {
+let charles = {
   poder:"psiquico",
   estatura: 1.78
 };
 
-var apocalipsis = {
+let apocalipsis = {
   lider:true,
   miembros: ["Magneto","Tormenta","Psylocke","Angel"]
 }
 
 // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-var mystique;
+let mystique;
 
 mystique = charles;
 mystique = apocalipsis;
